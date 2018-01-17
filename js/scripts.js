787e5981b467513f250813c612baae6c03427593
Brandon King <brandonking000@hotmail.com>
2018-01-16 19:38:57 -0500

    December 16, 2018 Commit
    -Added inline script to HTML to fix a flickering effect with the fade-in transition that occurs when the page first loads.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // reveal page content when page finishes loading
-
 window.addEventListener("load", function() {
     console.log("All resources finished loading");
     document.querySelector('body').style.opacity = 1;
