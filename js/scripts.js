8dd3ec0e31ffa790caafabb38d809775a64d9816
Brandon King <brandonking000@hotmail.com>
2018-01-16 20:25:18 -0500

    January 16, 2018 Commit
    -Trying a different fix to the flickering problem with the page on load.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-// reveal page content when page finishes loading
+// reveal content when page finishes loading
 window.addEventListener("load", function() {
     console.log("All resources finished loading");
-    document.querySelector('body').style.opacity = 1;
+    // document.querySelector('body').style.opacity = 1;
+    document.body.classList.remove('fade-out');
 });
 
 // document.addEventListener("DOMContentLoaded", function() {
